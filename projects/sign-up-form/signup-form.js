aac595ccf7c17bf68c598fb0048d1ea25c5ff330
Benjamin Yong <benj_yongzh@hotmail.com>
2022-10-13 22:29:39 +0800

    Add fucntionality of submit button. Have yet to fix phone number pattern, and styling of submission errors.

--- projects/sign-up-form/signup-form.js
+++ projects/sign-up-form/signup-form.js
@@ -4,13 +4,51 @@ const signupButton = document.querySelector("input[type='submit']");
 signupButton.addEventListener('click', onSubmit);
 
 function onSubmit(event){
-    validateInputs();
+    if(!validateInputs()) {
+        event.preventDefault();
+        return;
+    };
+
+
 }
 
 function validateInputs() {
     const formRows = form.querySelectorAll('.form-row');
     formRows.forEach(formRow => {
         const formInput = formRow.querySelector('input');
-        console.log(formInput);
-    })
+        //check values
+        
+        //check requireds
+        if (formInput.hasAttribute('required')) {
+            if (formInput.value == "") {
+                formInput.classList.add('error');
+                //return false;
+            }
+        }
+
+        const inputID = formInput.getAttribute("id");
+
+        //phone number check
+        if (inputID == "phone-number") {
+            if (formInput.value.length != 8) {
+                formInput.classList.add('error');
+                console.log("phone number should be 8 digits")
+                //return false;
+            };
+        }
+
+        //password check same
+        else if (inputID == "password") {
+            const confirmPasswordInput = form.querySelector('.form-row>input[name="userPasswordConfirm"]');
+            console.log(formInput.value.toString());
+            console.log(confirmPasswordInput.value.toString());
+            if (formInput.value.toString() !== confirmPasswordInput.value.toString()) {
+                formInput.classList.add('error');
+                confirmPasswordInput.classList.add('error');
+                console.log("passwords must be same")
+                //return false;
+            }
+        }
+
+    });
 }
