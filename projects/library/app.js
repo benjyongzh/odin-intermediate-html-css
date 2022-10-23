51630e4cb07da37424ddfe471f21323a1d198fa2
Benjamin Yong <benj_yongzh@hotmail.com>
2022-10-23 16:24:05 +0800

    Add 'add and remove' feature. Add read status toggling feature.

--- projects/library/app.js
+++ projects/library/app.js
@@ -1,5 +1,6 @@
 const container = document.querySelector('.container');
 const mainArea = document.querySelector('.main');
+const booksArea = mainArea.querySelector('.books');
 
 const mainBookAddButton = document.querySelector(".header>.button-add");
 mainBookAddButton.addEventListener('click', openSideBar);
@@ -20,7 +21,7 @@ function openSideBar(event) {
     sideBar.classList.add('adding-book');
     event.target.classList.add('adding-book');
     event.target.textContent = "Adding New Book...";
-    console.log("lmao open");
+    /* console.log("lmao open"); */
 };
 
 function closeSideBar(event) {
@@ -28,20 +29,67 @@ function closeSideBar(event) {
     sideBar.classList.remove('adding-book');
     mainBookAddButton.classList.remove('adding-book');
     mainBookAddButton.textContent = "Add New Book";
-    console.log("lmao open");
+    /* console.log("lmao close"); */
+    /* sideBarForm.reset(); */
 };
 
 function addNewBook(event){
-    //check if form fields are valid
+    //add new book
     event.preventDefault();
     const newBookData = new FormData(event.target);
     const formProps = Object.fromEntries(newBookData);
     console.log(formProps);
 
-
-    /* const newBook = document.createElement("div");
+    const newBook = document.createElement("div");
     newBook.classList.add('book');
+    booksArea.appendChild(newBook);
+
     const newBookTitle = document.createElement('div');
-    newBookTitle.classList.add('book-title'); */
-    // newBookTitle
+    newBookTitle.classList.add('book-title');
+    newBookTitle.textContent = formProps.bookName;
+    newBook.appendChild(newBookTitle);
+
+    const newBookButtonArea = document.createElement('div');
+    newBookButtonArea.classList.add('book-button-area');
+    newBook.appendChild(newBookButtonArea);
+
+    const newBookButtonRead = document.createElement('button');
+    newBookButtonRead.classList.add('book-button-read');
+    newBookButtonRead.textContent = 'Read';
+    newBookButtonRead.addEventListener('click', toggleReadStatus);
+    newBookButtonArea.appendChild(newBookButtonRead);
+
+    const newBookButtonRemove = document.createElement('button');
+    newBookButtonRemove.classList.add('book-button-remove');
+    newBookButtonRemove.textContent = 'Remove';
+    newBookButtonRemove.addEventListener('click', removeBook);
+    newBookButtonArea.appendChild(newBookButtonRemove);
+    
+    //check read status
+    if (formProps.bookReadStatus) {
+        newBook.classList.add('book-status-read');
+        newBookButtonRead.textContent = 'Un-read';
+    }
+
+    //clear form
+    event.target.reset();
+}
+
+function toggleReadStatus(event) {
+    //find book parent
+    const bookParent = event.target.parentElement.parentElement;
+    //toggle status
+    bookParent.classList.toggle('book-status-read');
+    if (event.target.textContent == "Read") {
+        event.target.textContent = "Un-read";
+    } else {
+        event.target.textContent = "Read";
+    };
+}
+
+function removeBook(event) {
+    //find book parent
+    const bookParent = event.target.parentElement.parentElement;
+    //delete book
+    bookParent.remove();
 }
